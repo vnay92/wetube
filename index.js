aa3e02e18ea62d9b46fea9a4afe4a426721af315
Vinay Bharadwaj <vinay@sellerworx.com>
2016-08-03 15:12:01 +0530

    removed ffmpeg just keeping flowplayer

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 var express = require('express');
-var ffmpeg = require('fluent-ffmpeg');
-
 var app = express();
 
 app.use(express.static(__dirname + '/static'));
@@ -13,20 +11,13 @@ app.get('/video/:filename', function(req, res) {
     res.contentType('mp4');
     // make sure you set the correct path to your video file storage
     var pathToMovie = '/home/vinay/Videos/' + req.params.filename;
-    var proc = ffmpeg(pathToMovie)
-        // use the 'flashvideo' preset (located in /lib/presets/flashvideo.js)
-        .preset('flashvideo')
-        // setup event handlers
-        .on('end', function() {
-            console.log('file has been converted succesfully');
-        })
-        .on('error', function(err) {
-            console.log('an error happened: ' + err.message);
-        })
-        // save to stream
-        .pipe(res, {
-            end: true
-        });
+    res.sendFile(pathToMovie, function (err) {
+        if(err) {
+            console.log('Error Occured');
+        } else {
+            console.log('Sent!');
+        }
+    });
 });
 
 app.listen(42069, function() {
