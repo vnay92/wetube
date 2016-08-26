4e73910c5dbd8e8627c9b523ca1ecce0da7357a8
Vinay Bharadwaj <vnay92@gmail.com>
2016-08-26 18:13:58 +0530

    added config

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
+var config = require('config.json');
 
-app.use(express.static(__dirname + '/static'));
+var BASE_DIR = config.BASE_DIR;
+
+app.use(express.static('/home/vinay/Videos'));
 
 app.get('/', function(req, res) {
     res.send('index.html');
@@ -10,10 +13,10 @@ app.get('/', function(req, res) {
 app.get('/video/:filename', function(req, res) {
     res.contentType('mp4');
     // make sure you set the correct path to your video file storage
-    var pathToMovie = '/home/vinay/Videos/' + req.params.filename;
+    var pathToMovie = BASE_DIR + req.params.filename;
     res.sendFile(pathToMovie, function (err) {
         if(err) {
-            console.log('Error Occured');
+            console.log('Error Occured', err);
         } else {
             console.log('Sent!');
         }
